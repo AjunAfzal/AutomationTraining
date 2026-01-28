6fe1bbf5b7f9637f75d8a69dd47bc02a6a9e8d4d
ADMIN <ADMIN@ACER>
2026-01-28 10:50:55 +0530

    A program to test private access modifier

---
+++ projectoops/src/main/java/privatemodifier/Fish.java
@@ -0,0 +1,13 @@
+package privatemodifier;
+
+public class Fish {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Try to call the private method from the animal class
+		Animal out1=new Animal();
+		//out1.speed();
+		//Cannot call like this as speed is a private method
+	}
+
+}
